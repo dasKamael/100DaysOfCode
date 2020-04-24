9bc8156b4f9f7ccc7736e1b3c0c53376cc40a2ab
dasKameltreiber <dasKameltreiber@users.noreply.github.com>
2020-04-24 16:33:39 +0200

    Day 11

--- Round1/D10/brew_crew/lib/services/auth.dart
+++ Round1/D10/brew_crew/lib/services/auth.dart
@@ -1,21 +1,32 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthService{
-
+class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on FirebaseUser
+  User _userFromFireBaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // Auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+        // .map((FirebaseUser user) => _userFromFireBaseUser(user)); is the same
+        .map(_userFromFireBaseUser);
+  }
 
   // Sign in anonymously
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
-    } catch(e) {
+      return _userFromFireBaseUser(user);
+    } catch (e) {
       print(e.toString());
       return null;
     }
   }
-
 
   // Sign in with email & password
 
